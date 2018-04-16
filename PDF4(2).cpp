cd7b45cd00fc16105ebf1436209c43d39d6b0806
LuisVillanuevaFlores <38118707+LuisVillanuevaFlores@users.noreply.github.com>
2018-04-16 07:42:08 -0500

    Create PDF4(2).cpp

---
+++ PDF4(2).cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+//2.1
+/*template < typename T>
+    mini( const T t1 , const T t2) {
+    return t1 < t2 ? t1 : t2;
+ }
+
+
+ int main(){
+    float a=5.23;
+    float b=6.23;
+    cout<<mini(a,b);
+    return 0;
+
+
+ }
+*/
+
+//2.2
+/*
+#define mini(x, y) (x < y ? x : y)
+int main(){
+    float a=5.23;
+    float b=6.32;
+    cout<<mini(a,b);
+    return 0;
+
+
+ }
+*/
+
+
+
+
+
